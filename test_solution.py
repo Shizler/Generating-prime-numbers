26f7b4772109868e00e26d6573d2f8f626fe3637
Shizler <win99nie@gmail.com>
2017-03-07 08:54:54 +0000

    Delete test_solution.py

--- test_solution.py
+++
@@ -1,6 +0,0 @@
-import unittest 
-class TestSolution(unittest.TestCase):
-	def check_if_number_is_an_interger(self):
-		numbers=prime_numbers("8")
-		self.assertTrue("This is not an interger")
-		pass
