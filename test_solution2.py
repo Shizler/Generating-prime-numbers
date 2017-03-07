223893fb44debabb2086e3be5a3123e7f115ad17
Shizler <win99nie@gmail.com>
2017-03-07 08:55:06 +0000

    Delete test_solution2.py

--- test_solution2.py
+++
@@ -1,6 +0,0 @@
-import unittest 
-class TestSolution(unittest.TestCase):
-	def check_if number_is_one_or_zero(self):
-		numbers=prime_numbers(0,1)
-		self.assertTrue("This is not a prime number")
-		pass
